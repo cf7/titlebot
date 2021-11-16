6cafb78d4dd5e51ebe9fb2d57974c9206a92f3d7
cf7 <cf7@users.noreply.github.com>
2021-11-16 15:02:29 -0800

    fixed processURL bug edge case

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -7,12 +7,6 @@ import { configure, shallow, mount, render } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import App from '../App.js';
 
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
-
 
 configure({ adapter: new Adapter() });
 
@@ -40,33 +34,23 @@ describe("Titlebot App", () => {
 
     it("extracts proper urls if they exist", () => {
       const wrapper = mount(<App />);
-      wrapper.setProps({ displayURL: 'asdfasdfhttps://example.comlkjlkjlkj' });
-      wrapper.find('input.submit-btn').simulate('click');
-      expect(wrapper.instance().processURL()).toBe('https://example.com');
+      expect(wrapper.instance().processURL('asdfasdfhttps://example.comlkjlkjlkj', ['.com'])).toBe('https://example.com');
       wrapper.unmount();
     });
 
-    it("fails when suffixes are missing", () => {
+    it("returns null when suffixes are missing", () => {
       const wrapper = mount(<App />);
-      wrapper.setProps({ displayURL: 'asdfasdfhttps://examplelkjlkjlkj' });
-      wrapper.find('input.submit-btn').simulate('click');
-      expect(wrapper.instance().processURL()).toBe('');
+      expect(wrapper.instance().processURL('asdfasdfhttps://examplelkjlkjlkj', ['.com'])).toBe('');
       wrapper.unmount();
     });
-
-    // it("displays retrieved title on page", () => {
-    //   const wrapper = mount(<App />);
-    //   wrapper.setState({ title: 'Fake Title' });
-    //   expect(wrapper.state)
-    //   wrapper.unmount();
-    // });
   });
 
   describe("Submit Button", () => {
     it("submits when clicked", async () => {
       const wrapper = mount(<App />);
       wrapper.instance().handleClick = jest.fn();
-      await wrapper.find('input.submit-btn').simulate('click');
+      await wrapper.update();
+      wrapper.find('input.submit-btn').simulate('click');
       expect(wrapper.instance().handleClick).toHaveBeenCalled();
       wrapper.unmount();
     });
