7019c7c47d699146ba14a4321930fcd31e94b9be
cf7 <cf7@users.noreply.github.com>
2021-11-16 03:43:19 -0800

    express server

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(morgan());
+
+
+app.get('/', (req, res) => {
+  res.status(200).send("Success!");
+});
+
+let port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
